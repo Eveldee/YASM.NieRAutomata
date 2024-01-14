ac1cf8c5a8af611ddc394cecbf6b29b8caa440c1
Eveldee <eveldee0680@live.fr>
2024-01-14 23:30:59 +0100

    Fix button id overlap

--- YASM.NieRAutomata/Overlays/ImportOverlay.cs
+++ YASM.NieRAutomata/Overlays/ImportOverlay.cs
@@ -32,7 +32,7 @@ public partial class MainOverlay
             {
                 var fileInfo = new FileInfo(savePath);
 
-                if (ButtonColored(TitleColor, "Import"))
+                if (ButtonColored(TitleColor, $"Import##{i}"))
                 {
                     BaseSaveInfo = SaveInfoManager.ExtractFromSave(i);
 
